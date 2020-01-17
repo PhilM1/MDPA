fe9eab5867abb0b4d4d918842aa336efd2fb09bc
Phil <pm993@hotmail.com>
2020-01-17 13:10:17 -0500

    Initial Commit with graph test

---
+++ MDPA.m
@@ -0,0 +1,15 @@
+
+springConst = 1;
+timestep = 0.1;
+
+particleArray = [1, 0, 1;
+                 2, 0, 1];
+y = [1, 1];
+             
+while(1)
+    
+    
+    
+    scatter(particleArray(:,1), y, 'filled');
+    pause(1)
+end
