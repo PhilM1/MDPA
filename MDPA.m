6dc4b49dae777bd59d840952046646838de3c69a
Phil <pm993@hotmail.com>
2020-01-17 14:34:07 -0500

    Final version, still buggy with force application at 10s

--- MDPA.m
+++ MDPA.m
@@ -1,39 +1,42 @@
 
 springConst = 1;
 timestep = 0.1;
-numParticles = 2;
+numParticles = 5;
 particleArray = [1, 0, 1;
-                 2, 0, 1];
+                 2, 0, 1
+                 3, 0, 1
+                 4, 0, 1
+                 5, 0, 1];
 x0Dist = 0.5;
-y = [1, 1];
-             
+y = ones(1,5);
+timeCount = 0;
 while(1)
-    
+    timeCount = timeCount + 1;
     %update velocities
     for i = 1:numParticles
         
         F1 = 0;
         F2 = 0;      
         
-        if(i ~= 1) %Left Side
+        if(i ~= 1) %Left Side force
             pDiff = abs(particleArray(i-1,1) - particleArray(i,1));
             F1 = springConst*(pDiff-x0Dist)^2;
-            if(pDiff < x0Dist)
+            if(pDiff > x0Dist)
                 F1 = -F1;
             end
         end
         
-        if(i ~= numParticles) %Right Side
-            pDiff = abs(particleArray(i+1,1) - particleArray(i,1));
-            F2 = springConst*(pDiff-x0Dist)^2;
-            if(pDiff > x0Dist)
+        if(i ~= numParticles) %Right Side force
+            pDiff2 = abs(particleArray(i+1,1) - particleArray(i,1));
+            F2 = springConst*(pDiff2-x0Dist)^2;
+            if(pDiff2 < x0Dist)
                 F2 = -F2;
             end
         end
         
         %time to update velocities
         F_total = F1 + F2;
-        accel = F1/particleArray(i,3);
+        accel = F_total/particleArray(i,3);
         newV = particleArray(i,2) + accel*timestep;
         particleArray(i,2) = newV;
     end
@@ -46,6 +49,13 @@ while(1)
     
     particleArray
     
+    %add perturbation at 10secs
+    if( timeCount*timestep == 10)
+        particleArray(5,2) = particleArray(i,2) + 1;
+    end
+    
     scatter(particleArray(:,1), y, 'filled');
-    pause(1)
+    title(['Current Time: ', num2str(timeCount*timestep)]);
+    xlim([0,10]);
+    pause(timestep);
 end
